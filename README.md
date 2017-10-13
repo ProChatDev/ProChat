fd60fa23d3d1730fda3f3192cea6d4ed54c2fb1d
JustMaffie <jorivanee@hotmail.com>
2017-10-13 13:48:50 +0200

    Update install.py and readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-`Help people interested in this repository understand your project by adding a README.` my ass
+# Hey, welcome to this repository
+
+## Installation:
+
+### Run `py install.py`, this will install all dependencies and copy the example configurations, then run `py migrator.py` in the `database` directory, wait until this is finished, then run `py app.py` in the backend directory, keep this running, then run `py gateway.py` in the gateway directory, keep this running, then run `npm start` in the frontend directory, keep this running, 
--- install.py
+++ install.py
@@ -5,6 +5,7 @@ import subprocess
 import asyncio
 from shutil import copyfile
 import pip
+import json
 
 _requirement_files = ["gateway\\requirements.txt", "backend\\requirements.txt", "database\\requirements.txt"]
 requirement_files = []
@@ -25,48 +26,110 @@ for key, val in _copyfiles.items():
 
 requirements = []
 
+configfiles = copyfiles.values()
+
+config = json.loads(open("install-config.json").read())
+
+def save_config():
+	with open("install-config.json", 'w') as outfile:
+	    json.dump(config, outfile, indent=4)
+
+# If it isn't pretty, this will make it pretty, if its already pretty, this is useless
+save_config()
+
 for file in requirement_files:
 	with open(file) as f:
 		lines = f.read().splitlines()
 		requirements.extend(lines)
 
 async def install_pip_requirements(requirements):
+	if not config.get("pip_install", True):
+		print("Not running the backend+gateway dependencies installation, its disabled in install-config.json")
+		return
 	installed_pkgs = [pkg.key for pkg in pip.get_installed_distributions()]
 	for requirement in requirements:
 		if requirement in installed_pkgs:
 			print("Requirement \"{}\" is already satisfied, not installing!".format(requirement))
 		else:
 			print("Installing \"{}\"".format(requirement))
-			result = await pip_install(requirement)
+			result = not bool(await pip_install(requirement))
 			if not result:
 				print("Failed to install {}: {}".format(requirement, result))
 			else:
 				print("Successfully installed {}: {}".format(requirement, result))
+	config['pip_install'] = False
+	save_config()
 
 async def pip_install(name, *, timeout=None):
 	# https://github.com/Cog-Creators/Red-DiscordBot/blob/develop/red.py#L186 thanks red, I modified it a little bit
-    args = [
-        "pip", "install",
-        "--upgrade",
-        name
-    ]
-    def install():
-        code = subprocess.call(args)
-        sys.path_importer_cache = {}
-        return not bool(code)
-    response = asyncio.get_event_loop().run_in_executor(None, install)
-    return await asyncio.wait_for(response, timeout=timeout)
+	args = [
+		"pip", "install",
+		"--upgrade",
+		name
+	]
+	return await execute_command(args, timeout)
+
+async def npm_install():
+	if not config.get("npm_install", True):
+		print("Not running the frontend dependencies installation, its disabled in install-config.json")
+		return
+	try:
+		print(await execute_command("(cd frontend && npm install)", shell=True))
+	except Exception as e:
+		print("Can't npm install", e)
+	config['npm_install'] = False
+	save_config()
+
+CONSTANTS = {
+	"sql_key": "sql_uri"
+}
+
+async def execute_command(command, *, timeout=None, shell=False):
+	# https://github.com/Cog-Creators/Red-DiscordBot/blob/develop/red.py#L186 thanks red, I modified it a little bit
+	def install():
+		code = subprocess.call(command, shell=shell)
+		sys.path_importer_cache = {}
+		return code
+	response = asyncio.get_event_loop().run_in_executor(None, install)
+	return await asyncio.wait_for(response, timeout=timeout)
 
 def copyFiles(files):
+	if not config.get("configs", True):
+		print("Not running the configs installation, its disabled in install-config.json")
+		return
 	for key, val in files.items():
 		if os.path.isfile(val):
 			print("File \"{}\" already exists, not copying".format(val))
 		else:
 			print("Copying \"{}\" to \"{}\"".format(key, val))
 			copyfile(key, val)
+	config['configs'] = False
+	save_config()
 
-copyFiles(copyfiles)
+def sql_url():
+	if not config.get("sql", True):
+		print("Not running the sql url installation, its disabled in install-config.json")
+		return
+	url = input("What SQL URI do you want to use?\n>")
+	print(url)
+	for file in configfiles:
+		data = json.loads(open(file).read())
+		data[CONSTANTS['sql_key']] = url
+		json.dump(data, open(file, "w"), indent=4)
+	config['sql'] = False
+	save_config()
+
+async def install():
+	print("Installing frontend dependencies...")
+	await npm_install()
+	print("Installing backend+gateway dependencies...")
+	await install_pip_requirements(requirements)
+	print("Copying configuration files...")
+	copyFiles(copyfiles)
+	print("Setting SQL URL...")
+	sql_url()
+
 
 loop = asyncio.get_event_loop()  
-loop.run_until_complete(install_pip_requirements(requirements))  
+loop.run_until_complete(install())  
 loop.close()  
