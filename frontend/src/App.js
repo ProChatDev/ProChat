658a7948e87c30d54c84ae5b054719a635fce847
Jacob-Noah <jacobnoahbusiness@gmail.com>
2017-10-17 02:33:15 -0400

    2 + 2 is 4. Minus 1, that's 3. Quick maffs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to ProChat</h1>
         </header>
         <Login />
         <br />
