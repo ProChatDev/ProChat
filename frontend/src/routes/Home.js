d1e559eb6c61b36fe5c922981e605d8b79e5c6c4
Jacob-Noah <jacobnoahbusiness@gmail.com>
2017-10-18 14:09:56 -0400

    Redirect to login page if not logged in.

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../components/Layout';
 
-export default () => (
-  <div>
-    <Layout />
-  </div>
-);
+export default class Home extends Component {
+  isLoggedIn() {
+    return (
+      localStorage.getItem('email') &&
+      localStorage.getItem('username') &&
+      localStorage.getItem('id') &&
+      localStorage.getItem('token')
+    );
+  }
+
+  render() {
+    if (!this.isLoggedIn()) {
+      return (<Redirect to="/login" />);
+    } else {
+      return (
+        <div>
+          <Layout />
+        </div>
+      );
+    }
+  }
+}
